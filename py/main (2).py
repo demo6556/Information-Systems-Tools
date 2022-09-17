7ab2e561ea67803d291458af18cc3d238f7adcf1
Ustinov Artem <112740399+demo6556@users.noreply.github.com>
2022-09-17 12:46:18 +0700

    Python
    .py

---
+++ py/main (2).py
@@ -0,0 +1,7 @@
+import time
+from tkinter import messagebox
+if __name__ == '__main__':
+    messagebox.showinfo('Useful Python', 'Вы долго смотрели в монитор, теперь посмотрите в окно.')
+    time.sleep(10)
+    messagebox.showinfo('Useful Python', 'Вы долго смотрели в монитор, теперь посмотрите в окно.')
+
