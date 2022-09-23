f38129a94f1a0abbe87d6d890499230792b4101a
Ustinov Artem <112740399+demo6556@users.noreply.github.com>
2022-09-24 00:18:58 +0700

    Удаление
    Удаление ненужных фалов

--- py/main (2).py
+++
@@ -1,7 +0,0 @@
-import time
-from tkinter import messagebox
-if __name__ == '__main__':
-    messagebox.showinfo('Useful Python', 'Вы долго смотрели в монитор, теперь посмотрите в окно.')
-    time.sleep(10)
-    messagebox.showinfo('Useful Python', 'Вы долго смотрели в монитор, теперь посмотрите в окно.')
-
